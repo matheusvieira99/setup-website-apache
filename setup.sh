1326d71131007ad5de9fff839b3d08eee6be86a4
Matheus Vieira <matheus-vieira12@hotmail.com>
2025-02-21 18:26:05 -0300

    fix: syntax for packages installs in AWS

--- setup.sh
+++ setup.sh
@@ -4,10 +4,14 @@ LOG_FILE="/var/log/setup.log"
 exec &> >(tee -a "$LOG_FILE")
 
 echo "Updating package list..."
-sudo apt-get update
+sudo yum -y update
 
 echo "Installing required packages..."
-sudo apt-get install -y apache2 git curl
+sudo yum -y install httpd git curl
+
+echo "Starting and enabling Apache (httpd)..."
+sudo systemctl enable httpd
+sudo systemctl start httpd
 
 echo "Removing old site folder (if exists)..."
 CLONE_DIR="/var/www/html/site"
@@ -18,11 +22,14 @@ REPO_URL="https://github.com/matheusvieira99/websitecompleted.git"
 sudo git clone "$REPO_URL" "$CLONE_DIR"
 
 echo "Setting up the website in Apache..."
-sudo chown -R www-data:www-data "$CLONE_DIR"
+sudo chown -R apache:apache "$CLONE_DIR"
 sudo chmod -R 755 "$CLONE_DIR"
 
+echo "Updating SELinux permissions (if enabled)..."
+sudo restorecon -R /var/www/html
+
 echo "Restarting Apache..."
-sudo systemctl restart apache2
+sudo systemctl restart httpd
 
 echo "Sending POST request..."
 curl -X POST -d "nome=Matheus de Paula Vieira" https://difusaotech.com.br/lab/aws/index.php
--- userdata.sh
+++ userdata.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 wget wget https://raw.githubusercontent.com/matheusvieira99/setup-website-apache/main/setup.sh -O /tmp/setup.sh
 chmod +x /tmp/setup.sh
 /tmp/setup.sh
